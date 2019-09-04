ed35b64e02d666300895a1ff51fffb32e87053cf
Pipaaa07 <47385831+Pipaaa07@users.noreply.github.com>
2019-09-03 21:05:02 -0300

    Add files via upload

---
+++ operacionesbasicas2.php
@@ -0,0 +1,113 @@
+<?php
+      
+	  $numeroUno        =$_POST["num1"];
+      $numeroDos        =$_POST["num2"];               /*declaras las variables*/
+	  $operacion        =$_POST["operacion"];
+
+      echo "<h1>Operaciones</h1>";
+      
+      /*
+	  $suma = $numeroUno + $numeroDos;
+      $resta = $numeroUno - $numeroDos;
+      $division = $numeroUno / $numeroDos;
+	  $producto = $numeroUno * $numeroDos;
+	  
+	  
+	  echo "la suma es $suma<br>"; 
+	  echo "la resta es $resta<br>";
+	  echo "la division es $division<br>";
+	  echo "el producto es $producto<br>"; 
+
+	  */
+	  
+	  
+	  
+	  /*
+	  if($operacion=="+"){
+		  $resultado = $numeroUno + $numeroDos;
+		  
+		  
+	  }else{
+		   if($operacion=="-"){
+			   $resultado = $numeroUno - $numeroDos;
+			   
+		     }else{
+				   if($operacion=="*"){
+					   $resultado = $numeroUno * $numeroDos;
+					   
+				     }else{
+						  if($operacion =="/"){
+							  
+							  $resultado = $numeroUno / $numeroDos;
+						 
+						 
+						     }else{
+								 
+								 $resultado="error";
+								 
+							     }
+						 
+						 
+						 
+						 
+					     }
+				 
+				 
+				 
+				 
+			     }
+		  
+		  
+		  
+	        }
+	     */
+		 
+		 switch($operacion){
+			 
+			 case "*":
+			         $resultado = $numeroUno * $numeroDos;
+					 break;
+					 
+			 case "+":
+                     $resultado = $numeroUno + $numeroDos;
+                     break;
+					 
+             case "/":
+                      $resultado = $numeroUno / $numeroDos;
+					  break;
+             
+            case "-":
+                     $resultado = $numeroUno - $numeroDos;
+                     break;
+            
+            default:
+                    echo "simbolo no valido";
+                    break;
+		 }					
+              					  
+		 
+		 
+		 
+	         if(isset($resultado)){
+			 
+	         echo "El resultado es $resultado";
+
+
+
+                }else{
+				 
+				      echo "No se pudo procesar el resultado";
+					  
+				     }
+
+
+
+
+
+
+
+
+
+
+
+?>
